94336b2c0f38c77edad3e149e708ced13e2282ad
maha-akter <maha.akter001@umb.edu>
2020-11-17 04:18:18 +0000

    add invalid login error message

--- admin-class.js
+++ admin-class.js
@@ -12,6 +12,25 @@ function runEvent(e) {
     console.log(e.type);
     var user_name = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+	
+	if(isValidUser(user_name,password)){
+		//ajax call to varify user is student or instaractor
+		if(isStudent()){
+			// display student info with projects submission info
+		}else{
+			//display all current students by default
+		}
+	}else{
+		document.getElementById("submit").insertAdjacentHTML("beforeBegin","<div style=\"color:red;\">User name or Password is incorrect</div>");
+	}
+	
     console.log(user_name);
     console.log(password);
+}
+
+function isValidUser(user_name,password){
+	//ajax call to back end for for user account validation.
+	//return invavalidvalid user for now
+	
+	return false;
 }
