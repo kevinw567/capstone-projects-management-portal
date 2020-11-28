a48099f48bc2526177d001e7c31727beacb789dc
xinyuanzheng <xinyuazheng1995@gmail.com>
2020-11-28 02:18:31 -0500

    Add Course table

--- project.sql
+++ project.sql
@@ -3,19 +3,19 @@ USE project_db;
 
 #Table to store student information
 CREATE TABLE users(
-id INT NOT NULL PRIMARY KEY,
-first_name VARCHAR(100) NOT NULL,
-last_name VARCHAR(100) NOT NULL,
-extra_details VARCHAR(100),
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(100) NOT NULL,
+-- extra_details VARCHAR(100),
 email VARCHAR(100) NOT NULL,
-authentication VARCHAR(100) NOT NULL 
+authentication VARCHAR(100) NOT NULL,
+role varchar(20) not NULL 
 );
 
-CREATE TABLE roles(
-role varchar(20) NOT NULL PRIMARY KEY,
-user_id INT,
-FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- CREATE TABLE roles(
+-- role varchar(20) NOT NULL,
+-- user_id INT,
+-- FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
 
 
 CREATE TABLE projects(
@@ -28,3 +28,20 @@ extra_details VARCHAR(255),
 user_id INT,
 FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+-- table for the course that professor created
+CREATE TABLE courses(
+    id VARCHAR(20) PRIMARY KEY,
+    course_number VARCHAR(20),
+    course_description VARCHAR(100),
+    professor VARCHAR(100)
+);
+-- table to store the projects, students in that course
+CREATE TABLE courses_info(
+    id VARCHAR(20),
+    student_id INT,
+    projects_id INT,
+    FOREIGN KEY (id) REFERENCES courses(id),
+    FOREIGN KEY (student_id) REFERENCES users(id),
+    FOREIGN KEY (projects_id) REFERENCES projects(id)
+)
